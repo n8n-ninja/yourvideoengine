b31c257efa7eed5bb379ab6f765f356f0a3d6028
n8Ninja <n8n.ninja@gmail.com>
2025-06-06 20:06:48 +0200

    2025-06-06 18:06:47

--- temporal-worker/src/worker.ts
+++ temporal-worker/src/worker.ts
@@ -1,55 +1,24 @@
-// @@@SNIPSTART money-transfer-project-template-ts-worker
-import { Worker, NativeConnectionOptions, NativeConnection } from "@temporalio/worker";
-import * as activities from './activities';
-import { namespace, taskQueueName } from './shared';
-import { getEnv } from './helpers';
+import { Worker, NativeConnection } from "@temporalio/worker"
+import * as activities from "./activities"
+import * as workflows from "./workflows"
 
 async function run() {
-  const { address, namespace, clientCert, clientKey, apiKey } = await getEnv();
+  const connection = await NativeConnection.connect({
+    address: process.env.TEMPORAL_ADDRESS || "",
+    tls: {},
+  })
 
-  let connectionOptions: NativeConnectionOptions = {
-    address: address,
-  };
-
-  if (clientCert && clientKey) {
-  // Configure mTLS authentication if certificate and key are provided
-  connectionOptions.tls = {
-      clientCertPair: {
-        crt: clientCert,
-        key: clientKey,
-      },
-    };
-  } else if (apiKey) {
-    // API key authentication
-    connectionOptions.tls = true;
-    connectionOptions.apiKey = apiKey;
-    connectionOptions.metadata = {
-      'temporal-namespace': namespace || 'default',
-    };
-  } else {
-    // No authentication
-    connectionOptions.tls = false;
-  }
-
-  // Create the connection
-  const connection = await NativeConnection.connect(connectionOptions);
-
-  // Register Workflows and Activities with the Worker and connect to
-  // the Temporal Service.
   const worker = await Worker.create({
     connection,
-    workflowsPath: require.resolve('./workflows'),
+    workflowsPath: require.resolve("./workflows"),
     activities,
-    namespace,
-    taskQueue: taskQueueName,
-  });
+    taskQueue: process.env.TEMPORAL_TASK_QUEUE || "default",
+  })
 
-  // Start accepting tasks from the Task Queue.
-  await worker.run();
+  await worker.run()
 }
 
 run().catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
-// @@@SNIPEND
+  console.error(err)
+  process.exit(1)
+})
